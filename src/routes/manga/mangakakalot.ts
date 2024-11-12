28cd8a65daedf66a5d69a9835356b00df5437cbf
Baha Striker <bahastriker@outlook.com>
2024-11-12 17:11:16 +0100

    Fix fetchChapterPages

--- src/routes/manga/mangakakalot.ts
+++ src/routes/manga/mangakakalot.ts
@@ -47,7 +47,7 @@ const routes = async (fastify: FastifyInstance, options: RegisterOptions) => {
 
     try {
       const res = await mangakakalot
-        .fetchChapterPages(chapterId)
+        .fetchChapterPages(chapterId, '')
         .catch((err: Error) => reply.status(404).send({ message: err.message }));
 
       reply.status(200).send(res);
